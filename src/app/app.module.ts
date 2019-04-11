be42eebcd4c424979462a494d04e15eed1a56db0
Gersen James <gersen.jorge@gmail.com>
2019-04-11 16:40:37 -0300

    Criando o módulo de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './autenticacao';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    LoginModule,
   ],
   providers: [
     ],
