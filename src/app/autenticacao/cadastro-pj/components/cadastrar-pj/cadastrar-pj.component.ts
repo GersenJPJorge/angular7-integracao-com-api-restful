dc23ff3cdb8a28557439ecf42288040bfdf35fc4
Gersen James <gersen.jorge@gmail.com>
2019-04-15 12:51:52 -0300

    Criando o validador de CPF

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material'; // para mensagens de erros
 import { Router } from '@angular/router';        // para totemento
 import { CadastroPj } from '../../models';
+import { CpfValidator } from '../../../../shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -27,7 +28,7 @@ export class CadastrarPjComponent implements OnInit {
   		nome: ['', [Validators.required, Validators.minLength(3)]],
   		email: ['', [Validators.required, Validators.email]],
   		senha: ['', [Validators.required, Validators.minLength(6)]],
-  		cpf: ['', [Validators.required]],
+  		cpf: ['', [Validators.required, CpfValidator]],
   		razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
   		cnpj: ['', [Validators.required]]
   	});
